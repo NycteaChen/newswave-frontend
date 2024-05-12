edb3ec7ff9397784e2f1f8479c516dc20a6b9d73
LOOFOO <107696112+LOOFOO@users.noreply.github.com>
2024-05-12 14:42:33 +0800

    feat(issue36): 取得雜誌文章列表

---
+++ components/NPagination.vue
@@ -0,0 +1,7 @@
+<template>
+  <n-button
+    ><div class="d-flex align-items-center">
+      <span class="">載入更多</span>
+      <img :src="requireImage('icon/more.svg')" /></div
+  ></n-button>
+</template>
