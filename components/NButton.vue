89a88db7a785040146b721450b7840ba6fc9052d
NycteaChen <jungturn01tw@gmail.com>
2024-05-12 22:15:06 +0800

    feat(nButton): 調整 nButton 參數與自訂樣式
    調整:
    
    1. 依照 UI kit 撰寫 NButton 不同顏色、大小、狀態的樣式
    
    2. 新增 NButton props，可支援 text、icon 等項目
    
    3. 新增 NSpin 元件，需要 loading 的地方可以使用
    
    4. 移除 _variables.scss 部分關於 bootstrap button 的變數
    
    5. 移除引入 bootstrap/scss/buttons

--- components/NButton.vue
+++ components/NButton.vue
@@ -1,50 +1,320 @@
 <template>
   <button
     type="button"
-    class="n-button btn"
-    :disabled="disabled"
-    :class="{
-      'btn-primary': type === 'primary',
-      'btn-secondary': type === 'secondary',
-      'btn-success': type === 'success',
-      'btn-info': type === 'info',
-      'btn-warning': type === 'warning',
-      'btn-danger': type === 'danger',
-      'btn-light': type === 'light',
-      'btn-dark': type === 'dark',
-      'btn-link': type === 'link'
-    }"
+    class="n-button fw-bold rounded-1"
+    :disabled="disabled || loading"
+    :class="btnClass"
   >
-    <template v-if="loading">
+    <div
+      class="d-flex align-items-center"
+      :class="{ 'flex-row-reverse': iconPosition === 'left' }"
+    >
       <span
-        class="spinner-border spinner-border-sm"
-        aria-hidden="true"
-      ></span>
-      <span
-        class="visually-hidden"
-        role="status"
-        >Loading...</span
+        class="btn-text flex-fill"
+        :class="size === 'sm' ? 'mx-1' : 'mx-4'"
       >
-    </template>
-    <slot v-else />
+        {{ text }}
+      </span>
+      <n-spin
+        v-if="loading"
+        is-small
+      />
+      <img
+        v-else
+        class="btn-icon"
+        :src="iconSrc"
+      />
+    </div>
   </button>
 </template>
 <script setup lang="ts">
-interface NBtnProps {
-  type?:
-    | 'primary'
-    | 'secondary'
-    | 'success'
-    | 'info'
-    | 'warning'
-    | 'danger'
-    | 'light'
-    | 'dark'
-    | 'link';
+export interface NBtnProps {
+  text: string;
+  /**
+   * 按鈕色
+   * purchase、secondary 僅能與 type: fill 搭配
+   * light 僅能與 type: outline 搭配
+   */
+  color?: 'primary' | 'secondary' | 'purchase' | 'accent' | 'light';
+  type?: 'fill' | 'outline';
   size?: 'sm' | 'md' | 'lg';
+  iconSrc?: string;
+  iconPosition?: 'left' | 'right';
   loading?: boolean;
   disabled?: boolean;
 }
 
-defineProps<NBtnProps>();
+const props = withDefaults(defineProps<NBtnProps>(), {
+  text: '',
+  color: 'primary',
+  size: 'md',
+  type: 'fill',
+  iconSrc: '',
+  iconPosition: 'right',
+  loading: false,
+  disabled: false
+});
+
+const btnClass = computed(() => {
+  const size = `btn-${props.size}`;
+  const style = `btn-${props.color}${props.type === 'fill' ? '' : '-outline'}`;
+  return `${size} ${style}`;
+});
 </script>
+<style lang="scss" scoped>
+.n-button {
+  outline: 0;
+  line-height: 24px;
+  transition: $btn-transition;
+
+  .btn-icon {
+    height: 18px;
+  }
+}
+
+// 大小間距
+.btn-lg {
+  padding: 12px 20px;
+  font-size: $btn-font-size-lg;
+}
+
+.btn-md {
+  padding: 8px 20px;
+  font-size: $btn-font-size;
+}
+
+.btn-sm {
+  padding: 4px 8px;
+  font-size: $btn-font-size-sm;
+
+  .btn-icon {
+    height: 12px;
+  }
+}
+
+// 按鈕實色、outline 樣式
+%btn-fill {
+  border-color: transparent !important;
+}
+
+%btn-outline {
+  background: transparent;
+}
+
+%btn-disabled {
+  box-shadow: none;
+  opacity: 1;
+  cursor: not-allowed;
+}
+
+%btn-primary-focus {
+  background: $primary;
+  box-shadow: 0 0 0 4px $blue-200;
+  color: $gray-100;
+}
+
+.btn-primary {
+  @extend %btn-fill;
+
+  background: $primary;
+  color: $gray-100;
+
+  &:disabled,
+  &:disabled:active {
+    @extend %btn-disabled;
+
+    background: $blue-100;
+    color: $blue-300;
+  }
+
+  &:hover:not([disabled]),
+  &:active:not([disabled]) {
+    background: $blue-500;
+    color: $gray-100;
+  }
+
+  &:focus-visible:not([disabled]) {
+    @extend %btn-primary-focus;
+  }
+}
+
+.btn-primary-outline {
+  @extend %btn-outline;
+
+  border: 1px solid $primary;
+  color: $primary;
+
+  &:disabled,
+  &:disabled:active {
+    @extend %btn-disabled;
+
+    border-color: $blue-100;
+    background: transparent;
+    color: $blue-100;
+  }
+
+  &:hover:not([disabled]),
+  &:active:not([disabled]) {
+    border: 1px solid $blue-500;
+    background: $blue-500;
+    color: $gray-100;
+  }
+
+  &:focus-visible:not([disabled]) {
+    @extend %btn-primary-focus;
+  }
+}
+
+%btn-secondary-focus {
+  background: $secondary;
+  box-shadow: 0 0 0 4px $blue-300;
+  color: $primary;
+}
+
+.btn-secondary {
+  @extend %btn-fill;
+
+  background: $secondary;
+  color: $primary;
+
+  &:disabled,
+  &:disabled:active {
+    @extend %btn-disabled;
+
+    background: $gray-200;
+    color: $gray-400;
+  }
+
+  &:hover:not([disabled]),
+  &:active:not([disabled]) {
+    background: $blue-400;
+    color: $primary;
+  }
+
+  &:focus-visible:not([disabled]) {
+    @extend %btn-secondary-focus;
+  }
+}
+
+%btn-accent-focus {
+  background: $danger;
+  box-shadow: 0 0 0 4px $red-200;
+  color: $gray-100;
+}
+
+.btn-accent {
+  @extend %btn-fill;
+
+  background: $danger;
+  color: $gray-100;
+
+  &:disabled,
+  &:disabled:active {
+    @extend %btn-disabled;
+
+    background: $red-100;
+    color: $red-300;
+  }
+
+  &:hover:not([disabled]),
+  &:active:not([disabled]) {
+    background: $red-500;
+    color: $gray-100;
+  }
+
+  &:focus-visible:not([disabled]) {
+    @extend %btn-accent-focus;
+  }
+}
+
+.btn-accent-outline {
+  @extend %btn-outline;
+
+  border: 1px solid $danger;
+  color: $danger;
+
+  &:disabled,
+  &:disabled:active {
+    @extend %btn-disabled;
+
+    border-color: $red-300;
+    background: transparent;
+    color: $red-100;
+  }
+
+  &:hover:not([disabled]),
+  &:active:not([disabled]) {
+    border: 1px solid $red-500;
+    background: $red-500;
+    color: $gray-100;
+  }
+
+  &:focus-visible:not([disabled]) {
+    @extend %btn-accent-focus;
+  }
+}
+
+%btn-light-focus {
+  background: $blue-200;
+  box-shadow: 0 0 0 4px $blue-100;
+  color: $gray-100;
+}
+
+.btn-light-outline {
+  @extend %btn-outline;
+
+  border: 1px solid $gray-300;
+  color: $gray-100;
+
+  &:disabled,
+  &:disabled:active {
+    @extend %btn-disabled;
+
+    border-color: $gray-600;
+    background: transparent;
+    color: $gray-600;
+  }
+
+  &:hover:not([disabled]),
+  &:active:not([disabled]) {
+    border: 1px solid $blue-400;
+    background: $blue-400;
+    color: $gray-100;
+  }
+
+  &:focus-visible:not([disabled]) {
+    @extend %btn-light-focus;
+  }
+}
+
+%btn-purchase-focus {
+  background: $success;
+  box-shadow: 0 0 0 4px $green-100;
+  color: $gray-100;
+}
+
+.btn-purchase {
+  @extend %btn-fill;
+
+  background: $success;
+  color: $gray-100;
+
+  &:disabled,
+  &:disabled:active {
+    @extend %btn-disabled;
+
+    background: $green-100;
+    color: $green-200;
+  }
+
+  &:hover:not([disabled]),
+  &:active:not([disabled]) {
+    background: $green-800;
+    color: $gray-100;
+  }
+
+  &:focus-visible:not([disabled]) {
+    @extend %btn-purchase-focus;
+  }
+}
+</style>
--- components/_pages/Home/GoUseButton.vue
+++ components/_pages/Home/GoUseButton.vue
@@ -4,30 +4,25 @@
     class="d-block w-100"
   >
     <n-button
-      class="w-100 text-primary-white border bg-transparent fw-bold px-md-3"
-    >
-      <div class="d-flex align-items-center">
-        <span class="go-text flex-fill fs-5 lh-sm">前往使用</span>
-        <img
-          class="ms-auto"
-          :src="requireImage('icon/ship.svg')"
-        />
-      </div>
-    </n-button>
+      class="w-100"
+      v-bind="props"
+      text="前往使用"
+      :icon-src="requireImage('icon/ship.svg')"
+    />
   </nuxt-link>
 </template>
-<script setup lang="ts"></script>
-<style lang="scss" scoped>
-::v-deep(.n-button) {
-  padding: 12px 24px;
-  min-width: 192px;
-  height: 48px;
-  border-color: $gray-300;
+<script lang="ts" setup>
+import type { NBtnProps } from '@/components/NButton.vue';
+
+interface GoUseBtnProps {
+  color?: NBtnProps['color'];
+  type?: NBtnProps['type'];
+  size?: NBtnProps['size'];
 }
 
-@include media-breakpoint-down(md) {
-  .go-text {
-    transform: translateX(11px);
-  }
-}
-</style>
+const props = withDefaults(defineProps<GoUseBtnProps>(), {
+  color: 'accent',
+  type: 'fill',
+  size: 'lg'
+});
+</script>
