512f44a335580b36c38937875cac40ad362583cc
LOOFOO <107696112+LOOFOO@users.noreply.github.com>
2024-05-14 00:06:43 +0800

    feat(issue36): 分頁元件製作

--- components/NPagination.vue
+++ components/NPagination.vue
@@ -1,7 +1,86 @@
 <template>
   <n-button
-    ><div class="d-flex align-items-center">
-      <span class="">載入更多</span>
-      <img :src="requireImage('icon/more.svg')" /></div
-  ></n-button>
+    class="w-100"
+    v-bind="props"
+    text="載入更多"
+    :icon-src="requireImage('icon/more.svg')"
+  />
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li
+        class="page-item"
+        :class="{ disabled: isFirstPage }"
+      >
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Previous"
+          @click.prevent="handlePreviousPage"
+        >
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item">
+        <a
+          class="page-link"
+          href="#"
+          >{{ currentPage }}</a
+        >
+      </li>
+      <li
+        class="page-item"
+        :class="{ disabled: isLastPage }"
+      >
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Next"
+          @click.prevent="handleNextPage"
+        >
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<script lang="ts" setup>
+import type { NBtnProps } from '@/components/NButton.vue';
+
+export interface NPaginationProps {
+  totalPages: number;
+  currentPage: number;
+  // eslint-disable-next-line no-unused-vars, vue/require-default-prop
+  onPageChange?: (page: number) => void;
+  color?: NBtnProps['color'];
+  type?: NBtnProps['type'];
+  size?: NBtnProps['size'];
+}
+
+const props = withDefaults(defineProps<NPaginationProps>(), {
+  color: 'primary',
+  type: 'fill',
+  size: 'lg'
+});
+
+const currentPageRef = ref(props.currentPage);
+
+const isFirstPage = computed(() => currentPageRef.value === 1);
+const isLastPage = computed(() => currentPageRef.value === props.totalPages);
+
+const emitPageChange = () => {
+  if (props.onPageChange) {
+    props.onPageChange(currentPageRef.value);
+  }
+};
+
+const handlePreviousPage = () => {
+  currentPageRef.value = Math.max(currentPageRef.value - 1, 1);
+  emitPageChange();
+};
+
+const handleNextPage = () => {
+  currentPageRef.value = Math.min(currentPageRef.value + 1, props.totalPages);
+  emitPageChange();
+};
+</script>
--- pages/magazine/[category].vue
+++ pages/magazine/[category].vue
@@ -3,7 +3,21 @@
   <h1>雜誌文章列表 5-2</h1>
 
   <nuxt-link :to="`/article/${articleId}`"><div>雜誌文章1</div></nuxt-link>
+  <NPagination
+    :total-pages="10"
+    :current-page="currentPage"
+    @page-change="handlePageChange"
+  />
 </template>
 <script setup lang="ts">
+import NPagination from '@/components/NPagination.vue';
+
 const articleId = ref<string>('M-123');
+
+const currentPage = ref(1);
+
+const handlePageChange = (page: number) => {
+  currentPage.value = page;
+  // 在這裡實現分頁相關的邏輯
+};
 </script>
