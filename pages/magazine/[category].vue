565b35d9eff0e3c93b755a29f21a1f8f246aaac6
LOOFOO <107696112+LOOFOO@users.noreply.github.com>
2024-05-17 22:57:18 +0800

    feat(issue36): 雜誌文章列表

--- pages/magazine/[category].vue
+++ pages/magazine/[category].vue
@@ -1,10 +1,50 @@
 <template>
-  <div>{{ $route.params.category }}</div>
   <h1>雜誌文章列表 5-2</h1>
 
   <nuxt-link :to="`/article/${$route.params.category}/${articleId}`">
     <div>{{ `${$route.params.category}文章` }}</div>
   </nuxt-link>
+  <section class="container d-flex p-3">
+    <div class="title-img me-4">
+      <img
+        src="https://s3-alpha-sig.figma.com/img/28f5/1841/4a4a211181a9778ab18e8509f1c601e3?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PNp9w9dAynMK-jDxXLiSsJDggsv~~bX~cGZz6TASYgpjBxc7wYZbVjJg0sunkMcYrIbleK~AdQB752ogwn2AH2fXmPyQ40C6ujelUi0azjNfvTRKoB1b72xDlJqlPsCb5X3rSNXrDLIwzZwtb-Y3bmPQuVDQDR3R5EEa2sGyrYQ6PAgGrlSWhqOtutC4ejSb2PaQV0xOxYETsl3VeFblpt1umgpvCyV~ktGfAw9s8~z6ssprkl6u4JLQnRYSJdf191KX5WYWVEUGkOo84vBCioPPF2DCj8-XFrd4Emv62dzdhM8r6F8FESDSUv6gYRag2BVSAND~MOC6lcRiESUywA__"
+        alt="Mimage"
+        class="img-fluid"
+      />
+    </div>
+    <div class="magazine-title">
+      <div class="mb-3 fw-bold fs-3">
+        {{ $route.params.category }}
+      </div>
+      <div class="fs-6">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quas
+        ipsum quasi porro nemo deserunt voluptates. Aspernatur eius adipisci
+        odit deleniti laborum illo. Hic minus nisi asperiores accusamus fuga
+        ipsum.*20
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <nuxt-link :to="`/article/${articleId}`">
+          <div class="card h-100">
+            <img
+              src="https://s3-alpha-sig.figma.com/img/a42b/d33f/1838a6959de079672cb7810c637cd8fc?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dmjm0N-IAHDN5IxAUNHYHS9CJs8EUE63tXXyPRWMKjLEU~IMHSNEDI55WYLWzxb0SZCVoK6-98RmgwyPJfTT8VkhgTLK2QIjUxBq-I5sKex61YIaz2JMbQe4u4CBk~eY6cnj1ZCfOAy4~BQ0LxwMyKDZzfBssEdZdDZTArEaC1vdOgFDXUIwz2kxuo~dpgFOQ3njlB9P0FPp~5mLSgwIkRync0sWoAhY0TzZkOwarG4F0qdHqn9ZTAM~kbdD7wjils~-6vCimXroVYL69v7Mz1sq1mi3uxbPoXZ6uYnc7RmXZDy7cDkT29D9tyQwBMRL7XVCqOzs0I4ee~nX84ElNA__"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">
+                探索未來科技趨勢:人工智慧、虛擬現實、區塊鏈和綠色創新的新革命
+              </h5>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+  <nuxt-link :to="`/article/${articleId}`"><div>雜誌文章1</div></nuxt-link>
   <NPagination
     :total-pages="20"
     :current-page="currentPage"
@@ -19,3 +59,13 @@ const handlePageChange = (page: number) => {
   currentPage.value = page;
 };
 </script>
+<style lang="scss" scoped>
+.title-img {
+  max-width: 120px;
+  height: 120px;
+}
+
+.magazine-title {
+  color: $primary;
+}
+</style>
