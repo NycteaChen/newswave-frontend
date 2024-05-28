56477f2116850d55c5638a46618062429540665e
ssuyun.chen <ssuyun.chen@udngroup.com>
2024-05-28 16:31:00 +0800

    feat(issue53): 新增 socket 測試

---
+++ pages/notify/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>NotufyView</div>
+</template>
+<script setup>
+import { io } from 'socket.io-client';
+
+let socket = {};
+
+onMounted(() => {
+  const URL = 'http://localhost:5173/';
+  socket = io(URL, { transports: ['websocket'] });
+  socket.on('connect', (data) => {
+    console.log(`Client ${socket.id}`);
+    console.log(data);
+  });
+});
+</script>
