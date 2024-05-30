426d29d97c8b800fdf225160ca8db19d7e95bdea
ssuyun.chen <ssuyun.chen@udngroup.com>
2024-05-30 17:18:03 +0800

    feat(issue53): 新增 socket query

--- pages/notify/index.vue
+++ pages/notify/index.vue
@@ -9,11 +9,16 @@ const notice = ref(null);
 
 onMounted(() => {
   const URL = 'http://localhost:5173/';
-  const socket = openSocket(URL);
+  const socket = openSocket(URL, {
+    query: {
+      userId: '66581de434dcc5b46c3b6'
+    }
+  });
   socket.on('notice', (data) => {
     if (data.action !== 'create') return;
     notice.value = data;
     console.log('notice:', notice.value);
   });
+  socket.emit('user agent', navigator.userAgent);
 });
 </script>
