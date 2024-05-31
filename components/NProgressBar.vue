956bb6fe8f44d021ddf97cb35d838464f74ff0b2
NycteaChen <jungturn01tw@gmail.com>
2024-05-31 12:54:16 +0800

    feat(nStep): 製作 nStep 元件
    調整:
    
    1. 新增 NProgressBar、nStep 元件
    
    2. 訂閱 checkout 頁加上 nStep & 按鈕測試

---
+++ components/NProgressBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="n-progress-bar progress"
+    role="progressbar"
+    :aria-valuenow="width"
+    aria-valuemin="0"
+    aria-valuemax="100"
+  >
+    <div
+      class="progress-bar"
+      :style="{ width: `${width}%` }"
+    />
+  </div>
+</template>
+<script lang="ts" setup>
+export interface NProgressBarProps {
+  width: number;
+}
+
+withDefaults(defineProps<NProgressBarProps>(), {
+  width: 0
+});
+</script>
--- pages/subscription-plan/checkout.vue
+++ pages/subscription-plan/checkout.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="pt-3 pt-md-4 container-xxl">訂閱 checkout 頁</div>
+  <div class="pt-3 pt-md-4 container-xxl">
+    <n-step v-model:step="currentStep" />
+
+    <section class="mt-3 mt-md-4">
+      <button @click="currentStep = 1">step 1</button>
+      <button @click="currentStep = 2">step 2</button>
+      <button @click="currentStep = 3">step 3</button>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,4 +15,6 @@ definePageMeta({
   middleware: ['auth'],
   layout: 'info'
 });
+
+const currentStep = ref<number>(1);
 </script>
