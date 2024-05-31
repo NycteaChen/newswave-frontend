a453f50febf0c8bff8a96c11f860f32ce321a913
ssuyun.chen <ssuyun.chen@udngroup.com>
2024-05-31 14:12:59 +0800

    feat(issue53): 調整 socket

--- pages/member/notification/personal.vue
+++ pages/member/notification/personal.vue
@@ -1,3 +1,25 @@
 <template>
   <div>我的通知</div>
+  {{ notice }}
 </template>
+<script setup>
+import openSocket from 'socket.io-client';
+
+const notice = ref(null);
+const userStorage = JSON.parse(localStorage.getItem('user')) || {};
+
+onMounted(() => {
+  const URL = 'http://localhost:5173/';
+  const socket = openSocket(URL, {
+    query: {
+      userId: userStorage.id
+    }
+  });
+  socket.on('notice', (data) => {
+    if (data.action !== 'create') return;
+    notice.value = data;
+    console.log('notice:', notice.value);
+  });
+  socket.emit('user agent', navigator.userAgent);
+});
+</script>
