3a24d9ae3298582bdc22a7825b7e2254e15432ea
NycteaChen <jungturn01tw@gmail.com>
2024-06-04 12:46:56 +0800

    feat(nTable): 製作 nTable 元件
    調整:
    
    1. 新增 table 元件，支援設定 header、body 內容

--- pages/member/subscription/order.vue
+++ pages/member/subscription/order.vue
@@ -1,3 +1,46 @@
 <template>
   <div>訂單記錄 6-5</div>
+  <n-table
+    :columns="tableColumn"
+    :data-source="tableList"
+  />
 </template>
+<script lang="ts" setup>
+const tableColumn = [
+  {
+    title: '訂單編號',
+    dataIndex: 'transactionId',
+    key: 'transactionId'
+  },
+  {
+    title: '方案名稱',
+    dataIndex: 'planName',
+    key: 'planName'
+  },
+  {
+    title: '付款狀態',
+    dataIndex: 'payStatus',
+    key: 'payStatus'
+  },
+  {
+    title: '到期日',
+    dataIndex: 'subscriptionExpiredAt',
+    key: 'subscriptionExpiredAt'
+  }
+];
+
+const tableList = [
+  {
+    transactionId: '123456789',
+    planName: '方案1',
+    payStatus: '已付款',
+    subscriptionExpiredAt: '2022-01-01'
+  },
+  {
+    transactionId: '987654321',
+    planName: '方案2',
+    payStatus: '未付款',
+    subscriptionExpiredAt: '2022-01-01'
+  }
+];
+</script>
