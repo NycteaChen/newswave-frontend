863cec74d41eea416418ffa1030021b171605a15
NycteaChen <jungturn01tw@gmail.com>
2024-06-16 01:08:18 +0800

    feat(navIconBtn): 新增 navIconBtn 元件
    需求:
    
    1. 依據設計稿製作元件
    
    調整:
    
    1. 新增 navIconBtn，僅先製作 hover 樣式
    
    2. 調整 header 資料夾

---
+++ components/Header/NavIconBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="nav-icon-btn rounded-1 d-flex justify-content-center align-items-center cursor-pointer"
+    @mouseover="isHover = !isMobile"
+    @mouseleave="isHover = false"
+  >
+    <img :src="requireImage(`icon/${icon}${isHover ? '-hover' : ''}.svg`)" />
+  </div>
+</template>
+<script setup lang="ts">
+interface NavIconBtnProps {
+  icon: string;
+}
+
+const isMobile = inject<any>('isMobile');
+
+withDefaults(defineProps<NavIconBtnProps>(), {
+  icon: ''
+});
+
+const isHover = ref<boolean>(false);
+</script>
+<style lang="scss" scoped>
+.nav-icon-btn {
+  padding: 8px 10px;
+  width: 40px;
+  height: 40px;
+  transition: all 0.2s ease-in-out;
+}
+
+@include media-breakpoint-up(md) {
+  .nav-icon-btn {
+    &:hover {
+      background: $blue-100;
+    }
+  }
+}
+</style>
