f6d324a03605327fd16132c1e6e1c0554de5a013
NycteaChen <jungturn01tw@gmail.com>
2024-07-01 17:48:42 +0800

    feat(followLoading): 追蹤加上 loading

--- pages/member/article/follow.vue
+++ pages/member/article/follow.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="member-follow mx-auto mx-md-0">
-    <p class="text-muted fs-sm">追蹤感興趣的新聞主題，NewsWave 將通知您最新相關主題文章，讓您掌握第一時間資訊！</p>
+    <p class="text-muted fs-sm">
+      您可以追蹤有興趣的新聞主題，NewsWave 將通知您最新相關主題文章，讓您掌握第一時間資訊！
+    </p>
     <p class="text-muted fs-sm">{{ `目前追蹤主題數：${follows.length}` }}</p>
     <div class="follow-container row row-cols-2 g-2 row-cols-sm-3">
       <div
@@ -10,20 +12,27 @@
       >
         <button
           type="button"
-          class="follow-btn w-100 rounded-2 is-btn gap-2 d-flex align-items-center justify-content-between"
+          class="follow-btn w-100 h-100 rounded-2 is-btn gap-2 d-flex align-items-center justify-content-between"
           :class="{ active: follows.includes(item.name) }"
           :style="{ '--color': item.color }"
           @click="toggleFollowBtn(item.name)"
         >
-          <svg-icon
-            class="btn-icon"
-            :name="`follow-${item.value}`"
+          <n-spin
+            v-if="loadingItem === item.name"
+            is-small
+            class="mx-auto"
           />
-          <span class="fs-6 fw-bold">{{ item.name }}</span>
-          <svg-icon
-            :name="follows.includes(item.name) ? 'minus' : 'plus'"
-            class="btn-icon"
-          />
+          <template v-else>
+            <svg-icon
+              class="btn-icon"
+              :name="`follow-${item.value}`"
+            />
+            <span class="fs-6 fw-bold">{{ item.name }}</span>
+            <svg-icon
+              :name="follows.includes(item.name) ? 'minus' : 'plus'"
+              class="btn-icon"
+            />
+          </template>
         </button>
       </div>
     </div>
@@ -36,6 +45,8 @@ definePageMeta({
 
 const userStore = useUserStore();
 const { follows } = storeToRefs(userStore);
+
+const loadingItem = ref<string>('');
 
 const { followNewsTopic, deleteFollowNewsTopic } = useUserApi();
 
@@ -62,18 +73,20 @@ const deleteFollowNewsTopicHandler = async (topic: string) => {
   }
 };
 
-function toggleFollowBtn(name: string) {
+const toggleFollowBtn = async (name: string) => {
+  loadingItem.value = name;
   if (follows.value?.includes(name)) {
-    deleteFollowNewsTopicHandler(name);
+    await deleteFollowNewsTopicHandler(name);
   } else {
-    followNewsTopicHandler(name);
+    await followNewsTopicHandler(name);
   }
-}
+  loadingItem.value = '';
+};
 </script>
 
 <style lang="scss" scope>
 .member-follow {
-  max-width: 550px;
+  max-width: 540px;
 }
 
 .follow-btn {
